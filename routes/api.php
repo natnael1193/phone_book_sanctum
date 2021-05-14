f53da9235edc18fe5bbc21c226234e20debc9596
Natnael Solomon <natnaelsolomon770@gmail.com>
2021-05-14 15:40:26 +0300

    change a user to subscriberId on api.php page line 71

--- routes/api.php
+++ routes/api.php
@@ -68,7 +68,7 @@ Route::post("/subscriber/login", function (Request $request) {
             'email' => ['The provided credentials are incorrect.'],
         ], 404);
     }
-    return ["user" => $subscriber->id, "token" =>$subscriber->createToken('my-token')->plainTextToken];
+    return ["subscriberId" => $subscriber->id, "token" =>$subscriber->createToken('my-token')->plainTextToken];
 });
 
 Route::post("/customer/login", function (Request $request) {
